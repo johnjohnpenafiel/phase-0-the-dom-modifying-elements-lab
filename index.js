bd7a29901810319d5b93f85a1fc3e76d19aa9990
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-04-10 20:00:18 -0400

    Modifying Elements Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'JOHN is the champion';
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
